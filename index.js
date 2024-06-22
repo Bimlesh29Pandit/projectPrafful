99e9035edd2cc375d36e31a0c2c567a1b6570e2e
Prafful Kumar <praffulkumarbkj2001@gmail.com>
2024-06-22 16:29:25 +0530

    add db connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connection  = require("./config/db");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -7,9 +8,10 @@ app.get("/", (req, res) => {
   res.send("Health check");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
   try {
-    console.log(`Connected to Server at Port ${PORT}`);
+    await connection;
+    console.log(`Connected to Server at Port ${PORT} and connected to DB`);
   } catch (error) {
     console.log("Something went wrong");
   }
